6ce19ffea291c27566b32bde8e149c4ef8e23249
Pedro Ivo <cinelli.dev@gmail.com>
2022-07-19 10:11:43 -0300

    chore. create basic project structure

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
+import router from "./routes/index.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(router)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Servidor aberto na porta ${PORT}`)
+})
